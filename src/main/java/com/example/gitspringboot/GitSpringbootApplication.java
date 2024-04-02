b88302821e7fcc695720065df19b51b274acc3b1
xiaofei <773211139@qq.com>
2024-04-02 13:26:44 +0800

    登录功能完成

--- src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
+++ src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
@@ -8,7 +8,7 @@ public class GitSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitSpringbootApplication.class, args);
-
+        System.out.println("开发登录功能");
     }
 
 }
