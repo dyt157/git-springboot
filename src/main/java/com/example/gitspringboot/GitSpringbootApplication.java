34f35c430c00433396c5e8252873bfde480f891c
邓宇桐 <76988236+dyt157@users.noreply.github.com>
2024-04-02 13:33:07 +0800

    Update GitSpringbootApplication.java

--- src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
+++ src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
@@ -8,11 +8,7 @@ public class GitSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitSpringbootApplication.class, args);
-        System.out.println("开发登录功能");
-        System.out.println("开发注册功能");
-        System.out.println("登录优化");
-        System.out.println("注册优化功能");
-        System.out.println("登录重构");
+        
     }
 
 }
