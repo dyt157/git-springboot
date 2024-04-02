4917c59fcdc1461a9ef064b7face8e0e4c57a274
xiaofei <773211139@qq.com>
2024-04-02 10:51:31 +0800

    测试开发完成

--- src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
+++ src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
@@ -8,7 +8,7 @@ public class GitSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitSpringbootApplication.class, args);
-        System.out.println("111111111111111111");
+        System.out.println("111111111111111333");
     }
 
 }
