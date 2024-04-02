32c5d7bf29ca8c127466ad72d7d4824cb509ac5e
xiaofei <773211139@qq.com>
2024-04-02 10:27:51 +0800

    初始工程

---
+++ src/main/java/com/example/gitspringboot/GitSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringbootApplication.class, args);
+        System.out.println("111111111111111111");
+    }
+
+}
